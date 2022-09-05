0ecd312c73fcf4f83d315d4693698804e0ad6f13
christophe <tremblay717@gmail.com>
2022-09-05 09:58:04 -0400

    added columns for editing library properties

--- backup.js
+++ backup.js
@@ -85,3 +85,114 @@ removeForm.textContent = "X"
 
 removeFormDiv.appendChild(removeForm);
 
+
+
+const newBook = document.querySelector('#addBook');
+
+// User enters book information in a form that appears when you click "ADD BOOK"
+const bookForm = document.createElement('form');
+
+// bookForm.addEventListener('submit', function (e) {
+//     e.preventDefault();
+// });
+
+// Form is split into 2 div; 
+
+// Left Div
+const bookUlLeft = document.createElement('ul');
+bookForm.appendChild(bookUlLeft);
+
+// Book Name Section
+const bookTitleLi = document.createElement('li');
+const bookTitleLabel = document.createElement('label');
+bookTitleLabel.textContent = "Book Title:";
+const bookTitleInput = document.createElement('input');
+bookTitleInput.id = "formBook"
+bookTitleInput.type = 'text';
+
+bookTitleLi.appendChild(bookTitleLabel);
+bookTitleLi.appendChild(bookTitleInput);
+bookUlLeft.appendChild(bookTitleLi);
+
+// Author's Name Section
+const authorLi = document.createElement('li');
+const authorLabel = document.createElement('label');
+authorLabel.textContent = "Author's Name:";
+const authorInput = document.createElement('input');
+authorInput.id = "formBook"
+authorInput.type = 'text';
+
+authorLi.appendChild(authorLabel);
+authorLi.appendChild(authorInput);
+bookUlLeft.appendChild(authorLi);
+
+// Right Div
+
+const bookUlRight = document.createElement('ul');
+bookForm.appendChild(bookUlRight);
+// Number of pages Section
+const bookPagesLi = document.createElement('li');
+const bookPagesLabel = document.createElement('label');
+bookPagesLabel.textContent = "Number of pages:"
+const bookPagesInput = document.createElement('input');
+bookPagesInput.id = "formBook"
+bookPagesInput.type = 'number';
+
+bookPagesLi.appendChild(bookPagesLabel);
+bookPagesLi.appendChild(bookPagesInput);
+bookUlRight.appendChild(bookPagesLi);
+
+// Reading Status
+const statusLi = document.createElement('li');
+const statusLabel = document.createElement('label');
+statusLabel.textContent = "Reading Status:"
+const readingInput = document.createElement('input');
+readingInput.type = "text";
+
+statusLi.appendChild(statusLabel);
+statusLi.appendChild(readingInput);
+bookUlRight.appendChild(statusLi);
+
+// Adding button so user can submit its book information
+
+const submitButton = document.createElement('button');
+submitButton.textContent = "Submit";
+bookForm.appendChild(submitButton);
+
+// Right Part Div with Remove Form "X"; Form  box will disapear from screen;
+const removeFormDiv = document.createElement('div');
+removeFormDiv.setAttribute('style', 'width:10%;padding-right:1%;')
+
+
+const removeForm = document.createElement('p');
+removeForm.style.cursor = "pointer";
+removeForm.style.textAlign = "right"
+removeForm.textContent = "X"
+
+removeFormDiv.appendChild(removeForm);
+
+
+
+const bookSection = document.getElementById('#bookSection');
+
+// Creating empty div 
+const enterBook = document.createElement('div');
+enterBook.className = "enterBook";
+
+const enterBookDiv = document.querySelector('#enterBookDiv');
+
+
+// newBook.onclick = function () {
+//     enterBook.appendChild(bookForm);
+//     enterBook.appendChild(removeFormDiv);
+//     enterBookDiv.appendChild(enterBook);
+// };
+
+
+removeForm.onclick = function () {
+    enterBookDiv.removeChild(enterBook);
+    bookTitleInput.value = "";
+    authorInput.value = "";
+    bookPagesInput.value = "";
+    readingInput.value = "";
+};
--- script.js
+++ script.js
@@ -1,113 +1,5 @@
 const newBook = document.querySelector('#addBook');
 
-// User enters book information in a form that appears when you click "ADD BOOK"
-const bookForm = document.createElement('form');
-
-// bookForm.addEventListener('submit', function (e) {
-//     e.preventDefault();
-// });
-
-// Form is split into 2 div; 
-
-// Left Div
-const bookUlLeft = document.createElement('ul');
-bookForm.appendChild(bookUlLeft);
-
-// Book Name Section
-const bookTitleLi = document.createElement('li');
-const bookTitleLabel = document.createElement('label');
-bookTitleLabel.textContent = "Book Title:";
-const bookTitleInput = document.createElement('input');
-bookTitleInput.id = "formBook"
-bookTitleInput.type = 'text';
-
-bookTitleLi.appendChild(bookTitleLabel);
-bookTitleLi.appendChild(bookTitleInput);
-bookUlLeft.appendChild(bookTitleLi);
-
-// Author's Name Section
-const authorLi = document.createElement('li');
-const authorLabel = document.createElement('label');
-authorLabel.textContent = "Author's Name:";
-const authorInput = document.createElement('input');
-authorInput.id = "formBook"
-authorInput.type = 'text';
-
-authorLi.appendChild(authorLabel);
-authorLi.appendChild(authorInput);
-bookUlLeft.appendChild(authorLi);
-
-// Right Div
-
-const bookUlRight = document.createElement('ul');
-bookForm.appendChild(bookUlRight);
-// Number of pages Section
-const bookPagesLi = document.createElement('li');
-const bookPagesLabel = document.createElement('label');
-bookPagesLabel.textContent = "Number of pages:"
-const bookPagesInput = document.createElement('input');
-bookPagesInput.id = "formBook"
-bookPagesInput.type = 'number';
-
-bookPagesLi.appendChild(bookPagesLabel);
-bookPagesLi.appendChild(bookPagesInput);
-bookUlRight.appendChild(bookPagesLi);
-
-// Reading Status
-const statusLi = document.createElement('li');
-const statusLabel = document.createElement('label');
-statusLabel.textContent = "Reading Status:"
-const readingInput = document.createElement('input');
-readingInput.type = "text";
-
-statusLi.appendChild(statusLabel);
-statusLi.appendChild(readingInput);
-bookUlRight.appendChild(statusLi);
-
-// Adding button so user can submit its book information
-
-const submitButton = document.createElement('button');
-submitButton.textContent = "Submit";
-bookForm.appendChild(submitButton);
-
-// Right Part Div with Remove Form "X"; Form  box will disapear from screen;
-const removeFormDiv = document.createElement('div');
-removeFormDiv.setAttribute('style', 'width:10%;padding-right:1%;')
-
-
-const removeForm = document.createElement('p');
-removeForm.style.cursor = "pointer";
-removeForm.style.textAlign = "right"
-removeForm.textContent = "X"
-
-removeFormDiv.appendChild(removeForm);
-
-
-
-const bookSection = document.getElementById('#bookSection');
-
-// Creating empty div 
-const enterBook = document.createElement('div');
-enterBook.className = "enterBook";
-
-const enterBookDiv = document.querySelector('#enterBookDiv');
-
-
-// newBook.onclick = function () {
-//     enterBook.appendChild(bookForm);
-//     enterBook.appendChild(removeFormDiv);
-//     enterBookDiv.appendChild(enterBook);
-// };
-
-
-removeForm.onclick = function () {
-    enterBookDiv.removeChild(enterBook);
-    bookTitleInput.value = "";
-    authorInput.value = "";
-    bookPagesInput.value = "";
-    readingInput.value = "";
-};
-
 // Object constructor 
 function Book(title, author, pages, status) {
     this.title = title;
@@ -127,7 +19,7 @@ const myLibrary = [hobbit, harryPotter, dune];
 const bookList = document.querySelector('#bookList');
 const bookGrid = document.createElement('div');
 bookGrid.id = "booKGrid";
-bookGrid.setAttribute('style', 'display: grid; border-style:solid; border-color:black; border-width:2px; background-color: #F3F3F3;padding:0px;')
+bookGrid.setAttribute('style', 'display: grid; border-style:solid; border-color:black; border-width:2px; background-color: #F3F3F3;padding:0px;');
 
 
 // function to add a header to our book Grid
@@ -140,9 +32,9 @@ function bookGridHeader() {
     bookHeader.className = "addingBook";
 
     // h3 element for our book title
-    titleHeader = document.createElement('h3')
-    titleHeader.textContent = "Book Title"
-    titleHeader.className = 'boxHeader'
+    titleHeader = document.createElement('h3');
+    titleHeader.textContent = "Book Title";
+    titleHeader.className = 'boxHeader';
 
     // h4 element for author's name
     authorName = document.createElement('h4');
@@ -151,20 +43,33 @@ function bookGridHeader() {
 
 
     // h4 element for number of pages
-    pages = document.createElement('h4')
+    pages = document.createElement('h4');
     pages.textContent = "Number of pages";
     pages.className = 'boxHeader';
 
     // h4 element for reading Status
     readingStatus = document.createElement('h4')
     readingStatus.textContent = "Reading Status";
-    readingStatus.className = 'boxHeader'
+    readingStatus.className = 'boxHeader';
+
+    // h4 element for removing book
+    removingBook = document.createElement('h4')
+    removingBook.textContent = "Remove Book?";
+    removingBook.className = 'boxHeader';
+
+    // h4 element for editing reading status
+    editReading = document.createElement('h4')
+    editReading.textContent = "Change reading status?";
+    editReading.className = 'boxHeader';
 
 
     bookHeader.appendChild(titleHeader);
     bookHeader.appendChild(authorName);
     bookHeader.appendChild(pages);
     bookHeader.appendChild(readingStatus);
+    bookHeader.appendChild(removingBook);
+    bookHeader.appendChild(editReading);
+
     bookGrid.appendChild(bookHeader);
 
 }
@@ -179,63 +84,80 @@ function createBookLinesLibrary() {
 
 
         // h3 element for our book title
-        addBookTitle = document.createElement('h3')
+        addBookTitle = document.createElement('h3');
         addBookTitle.textContent = myLibrary[i].title;
-        addBookTitle.setAttribute('style', 'border-right-style:solid;border-right-width:1px')
-        addBookTitle.className = 'box'
+        addBookTitle.id = myLibrary[i].title;
+        addBookTitle.setAttribute('style', 'border-right-style:solid;border-right-width:1px');
+        addBookTitle.className = 'box';
 
         // h4 element for author's name
-        addBookAuthor = document.createElement('h4')
+        addBookAuthor = document.createElement('h4');
         addBookAuthor.textContent = myLibrary[i].author;
-        addBookAuthor.setAttribute('style', 'border-right-style:solid;border-right-width:1px')
-        addBookAuthor.className = 'box'
+        addBookAuthor.setAttribute('style', 'border-right-style:solid;border-right-width:1px');
+        addBookAuthor.className = 'box';
 
 
         // h4 element for number of pages
-        addBookPages = document.createElement('h4')
+        addBookPages = document.createElement('h4');
         addBookPages.textContent = myLibrary[i].pages + " pages";
-        addBookPages.setAttribute('style', 'border-right-style:solid;border-right-width:1px')
-        addBookPages.className = 'box'
+        addBookPages.setAttribute('style', 'border-right-style:solid;border-right-width:1px');
+        addBookPages.className = 'box';
 
 
         // h4 element for reading Status
-        addreadingStatus = document.createElement('h4')
+        addreadingStatus = document.createElement('h4');
         addreadingStatus.textContent = myLibrary[i].status;
-        addreadingStatus.className = 'box'
+        addreadingStatus.setAttribute('style', 'border-right-style:solid;border-right-width:1px');
+        addreadingStatus.className = 'box';
+
+
+        // h4 element for removing book
+        removingBook = document.createElement('h4');
+        removingBook.id = myLibrary[i].title;
+        removingBook.setAttribute('style', 'border-right-style:solid;border-right-width:1px');
+        removingBook.textContent = "X";
+        removingBook.className = 'box';
+
+        // h4 element for editing reading status
+        editReading = document.createElement('h4');
+        editReading.id = myLibrary[i].title;
+        editReading.textContent = "Yes";
+        editReading.className = 'box';
 
         // No bottom Border for boxes in last row
         if (i != myLibrary.length - 1) {
-            addingBook.setAttribute('style', 'border-bottom-style:solid;border-bottom-width:1px;')
-        }
+            addingBook.setAttribute('style', 'border-bottom-style:solid;border-bottom-width:1px;');
+        };
 
         bookGrid.appendChild(addingBook);
         addingBook.appendChild(addBookTitle);
         addingBook.appendChild(addBookAuthor);
         addingBook.appendChild(addBookPages);
         addingBook.appendChild(addreadingStatus);
+        addingBook.appendChild(removingBook);
+        addingBook.appendChild(editReading);
+
     }
 }
-
-
 
 const odin = document.querySelector("#odin");
 
 newBook.onclick = function () {
     const livre = new Book();
 
-    const title = prompt("title");
-    const author = prompt("author");
-    const pages = prompt("pages");
-    const status = prompt("status");
+    const title = prompt("Enter Book Title");
+    const author = prompt("Enter Author's Name");
+    const pages = prompt("Enter Number of pages");
+    const status = prompt("Have you read the book?");
 
-    livre.title = title
-    livre.author = author
-    livre.pages = pages
-    livre.status = status
+    livre.title = title;
+    livre.author = author;
+    livre.pages = pages;
+    livre.status = status;
 
     myLibrary.push(livre);
 
-    // We make sure to clear our book Grid of every element. 
+    // We make sure to clear our book Grid of every element named after the addingBook Class. 
     let i = 0;
     while (i < document.querySelectorAll('.addingBook').length) {
         let addingBookClass = document.querySelector('.addingBook');
